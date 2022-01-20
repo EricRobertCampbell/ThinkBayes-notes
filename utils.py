20ad5ae8eeda7d8f78df1593909b8bcaa24512c9
EricRobertCampbell <eric.robert.campbell@gmail.com>
2022-01-19 20:32:55 -0700

    Finished Chapter 14

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from empiricaldist import Pmf
 
+# distributions
+from scipy.stats import binom
+
 def normalize(joint):
     """ Normalize a joint distribution """
     prob_data = joint.to_numpy().sum()
@@ -30,5 +33,54 @@ def marginal(joint, axis):
     axis=1: return the distribution of the second variable
     """
     return Pmf(joint.sum(axis=axis))
+
+def make_uniform(basis):
+    """ Make and return a uniform distribution on basis """
+    pmf = Pmf(1, basis)
+    pmf.normalize()
+    return pmf
     
-    
+def make_joint(s1, s2):
+    """Compute the outer product of two Series.
+    First Series goes across the columns;
+    second goes down the rows.
+    s1: Series
+    s2: Series
+    return: DataFrame
+    """
+    X, Y = np.meshgrid(s1, s2)
+    return pd.DataFrame(X*Y, columns=s1.index, index=s2.index) 
+
+def make_binomial(n, p):
+    """Make a binomial distribution.
+    n: number of trials
+    p: probability of success
+    returns: Pmf representing the distribution of k
+    """
+    ks = np.arange(n+1)
+    ps = binom.pmf(ks, n, p)
+    return Pmf(ps, ks)
+
+def make_mixture(pmf, pmf_seq):
+    """Make a mixture of distributions.
+    pmf: mapping from each hypothesis to its probability
+         (or it can be a sequence of probabilities)
+    pmf_seq: sequence of Pmfs, each representing
+             a conditional distribution for one hypothesis
+    returns: Pmf representing the mixture
+    """
+    df = pd.DataFrame(pmf_seq).fillna(0).transpose()
+    df *= np.array(pmf)
+    total = df.sum(axis=1)
+    return Pmf(total)
+
+def pmf_from_dist(dist, qs):
+    """Make a discrete approximation.
+    dist: SciPy distribution object
+    qs: quantities
+    returns: Pmf
+    """
+    ps = dist.pdf(qs)
+    pmf = Pmf(ps, qs)
+    pmf.normalize()
+    return pmf
